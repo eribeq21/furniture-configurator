0ebda9b8f88494fff467508423c75fdc6232df61
Eriseldi <eribeq21@htl-shkoder.com>
2026-03-28 15:58:07 +0100

    feat: basic 3D scene with camera, lights and GLB model loading

---
+++ src/lib/components/Furniture.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { T } from '@threlte/core';
+	import { useGltf } from '@threlte/extras';
+
+	const gltf = useGltf('/furniture.glb');
+</script>
+
+{#await gltf then model}
+	<T is={model.scene} />
+{/await}
---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { T } from '@threlte/core';
+	import { OrbitControls } from '@threlte/extras';
+	import Furniture from './Furniture.svelte';
+</script>
+
+<T.PerspectiveCamera makeDefault position={[3, 2, 5]} fov={50}>
+	<OrbitControls />
+</T.PerspectiveCamera>
+
+<T.AmbientLight intensity={0.5} />
+<T.DirectionalLight position={[5, 10, 5]} intensity={1} />
+
+<Furniture />
