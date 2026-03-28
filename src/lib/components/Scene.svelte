8fd1f94b030d3878a9d8bf3623b020dbb5322e23
Eriseldi <eribeq21@htl-shkoder.com>
2026-03-29 05:23:03 +0800

    feat: Add small circular shadow undr the base of the furniture

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -4,20 +4,35 @@
 	import Furniture from './Furniture.svelte';
 </script>
 
-<T.PerspectiveCamera makeDefault position={[3, 2, 5]} fov={50}>
-	<OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
+<T.PerspectiveCamera makeDefault position={[4, 3, 4]} fov={50}>
+	<OrbitControls
+		target={[0, -0.2, 0]}
+		enablePan={false}
+		minPolarAngle={0.3}
+		maxPolarAngle={1.3}
+		minDistance={3}
+		maxDistance={10}
+		enableDamping
+		dampingFactor={0.05}
+		autoRotate
+		autoRotateSpeed={0.5}
+	/>
 </T.PerspectiveCamera>
 
 <Environment url="/ferndale_studio_01_4k.hdr" />
-
+<T.Mesh position={[0, -0.93, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+	<T.CircleGeometry args={[0.8, 64]} />
+	<T.MeshStandardMaterial color="#000000" transparent opacity={0.5} roughness={1} metalness={0} />
+</T.Mesh>
 <!-- Fake soft ground shadow -->
 <ContactShadows
 	position={[0, -1.01, 0]}
+	frames={200}
 	scale={15}
 	blur={2.5}
 	opacity={0.2}
 	resolution={256}
-  color="#000000"
+	color="#000000"
 />
 
 <!-- Position of the furniture -->
